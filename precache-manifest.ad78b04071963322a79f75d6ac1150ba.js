2e3fc6df7362f2202f134000a288cd81ea8b2174
UncleLev <risentean@gmail.com>
2020-02-14 13:06:47 +0200

    Updates

---
+++ precache-manifest.ad78b04071963322a79f75d6ac1150ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff106484a1d9e7237666596d4fe33c98",
+    "url": "/StarDB/index.html"
+  },
+  {
+    "revision": "f172dcd6d084e0daed74",
+    "url": "/StarDB/static/css/main.7c2eadce.chunk.css"
+  },
+  {
+    "revision": "d309394d9fc845339377",
+    "url": "/StarDB/static/js/2.0c1dfcdb.chunk.js"
+  },
+  {
+    "revision": "25ab7760a9d196e1bee67b6bdb56e1bd",
+    "url": "/StarDB/static/js/2.0c1dfcdb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f172dcd6d084e0daed74",
+    "url": "/StarDB/static/js/main.c0df859a.chunk.js"
+  },
+  {
+    "revision": "5ad354ad4316e1a91277",
+    "url": "/StarDB/static/js/runtime-main.36a0d1cc.js"
+  }
+]);
